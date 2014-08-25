5fcab4257d495d8bc086f22f83b7e48aa537feaf
xuelan.mei <sharonmei1219@gmail.com>
2014-08-25 15:13:36 +0800

    multiSolutionFinder and Collector

--- src/puzzleGenerator.py
+++ src/puzzleGenerator.py
@@ -1,6 +1,7 @@
 class PuzzleGenerator:
-	def __init__(self, puzzleFactory):
+	def __init__(self, puzzleFactory, puzzleSolver):
 		self.puzzleFactory = puzzleFactory
+		self.puzzleSolver = puzzleSolver
 
 	def constructPuzzleWithOnlySolution(self, table, initNumCnt):
 		pos = self.randomPos(initNumCnt)
@@ -14,9 +15,50 @@ class PuzzleGenerator:
 
 		return puzzle
 
+	def solve(self, puzzle):
+		return self.puzzleSolver.solve(puzzle)
+
 	def createPuzzle(self, table, pos):
 		return self.puzzleFactory.createPuzzleFromTable(table, pos)
 
 	def randomPos(self, count):
 		return self.puzzleFactory.getRandomPos(count)
 
+class MultiSolutionSolver:
+	def __init__(self, solutionFinder):
+		self.solutionFinder = solutionFinder
+
+	def solve(self, puzzle):
+		solutionCollection = self.newSolutionCollections()
+		self.solutionFinder.solve(puzzle, solutionCollection)
+		return solutionCollection.result()
+
+class MultisolutionCollector:
+	def __init__(self):
+		self.solutionCount = 0
+		self.solutions = []
+
+	def record(self, puzzle):
+		self.solutions = self.solutions + [puzzle]
+		self.solutionCount += 1
+
+	def result(self):
+		if self.solutionCount is 2:
+			return MultiSolutionResult(self.solutionCount, self.solutions[0].compare(self.solutions[1]))
+
+		return MultiSolutionResult(self.solutionCount)
+
+	def done(self):
+		return self.solutionCount is 2
+
+class MultiSolutionResult:
+	def __init__(self, count, differencePos = None):
+		self.count = count
+		self.difference = differencePos
+
+	def solutionCount(self):
+		return self.count;
+
+	def solutionDifference(self):
+		return self.difference
+				
--- test/testPuzzleGenerator.py
+++ test/testPuzzleGenerator.py
@@ -1,5 +1,5 @@
 import unittest
-from puzzleGenerator import PuzzleGenerator
+from puzzleGenerator import *
 from unittest.mock import MagicMock
 from unittest.mock import call
 
@@ -12,7 +12,8 @@ class TestPuzzleGenerator(unittest.TestCase):
 		self.table = MockObject()
 		self.puzzle_1 = MockObject()
 		self.puzzle_2 = MockObject()
-		self.puzzleGen = PuzzleGenerator(self.factory)
+		self.solver = MockObject()
+		self.puzzleGen = PuzzleGenerator(self.factory, self.solver)
 		self.result_1 = MockObject()
 		self.result_2 = MockObject()
 		
@@ -20,20 +21,20 @@ class TestPuzzleGenerator(unittest.TestCase):
 
 	def test_constructPuzzleWithOnlyOneSolutionWithOneTry(self):
 		self.factory.createPuzzleFromTable = MagicMock(return_value=self.puzzle_1)
-		self.puzzleGen.solve = MagicMock(return_value=self.result_1)
+		self.solver.solve = MagicMock(return_value=self.result_1)
 		self.result_1.solutionCount = MagicMock(return_value = 1)
 
 		puzzle = self.puzzleGen.constructPuzzleWithOnlySolution(self.table, 1)
 
 		self.factory.getRandomPos.assert_called_once_with(1)
 		self.factory.createPuzzleFromTable.assert_called_once_with(self.table, [(0, 1)])
-		self.puzzleGen.solve.assert_called_once_with(self.puzzle_1)
+		self.solver.solve.assert_called_once_with(self.puzzle_1)
 		self.result_1.solutionCount.assert_called_once_with()
 		self.assertEquals(self.puzzle_1, puzzle)
 
 	def test_constructPuzzleWithOnlyOneSolutionWith2ndTry(self):
 		self.factory.createPuzzleFromTable = MagicMock(side_effect=[self.puzzle_1, self.puzzle_2])
-		self.puzzleGen.solve = MagicMock(side_effect=[self.result_1, self.result_2])
+		self.solver.solve = MagicMock(side_effect=[self.result_1, self.result_2])
 		self.result_1.solutionCount = MagicMock(return_value = 2)
 		self.result_1.solutionDifference = MagicMock(return_value = (2, 3))
 		self.result_2.solutionCount = MagicMock(return_value = 1)
@@ -41,9 +42,55 @@ class TestPuzzleGenerator(unittest.TestCase):
 		puzzle = self.puzzleGen.constructPuzzleWithOnlySolution(self.table, 1)
 
 		self.assertEquals([call(self.table, [(0, 1)]), call(self.table, [(0, 1), (2, 3)])],self.factory.createPuzzleFromTable.mock_calls)
-		self.assertEquals([call(self.puzzle_1), call(self.puzzle_2)], self.puzzleGen.solve.mock_calls)
+		self.assertEquals([call(self.puzzle_1), call(self.puzzle_2)], self.solver.solve.mock_calls)
 		self.assertEquals(self.puzzle_2, puzzle)
 
-class TestPuzzleGeneratorSolvePuzzle(unittest.TestCase):
-	def test_solvePuzzle(self):
-		pass
+class TestMutiSolutionSolver(unittest.TestCase):
+	def test_OnlyOneSolutionAvailable(self):
+		solutionFinder = MockObject()
+		solutionFinder.solve = MagicMock()
+		puzzle = MagicMock()
+		collection = MagicMock()
+		collectedResult = MagicMock()
+		solver = MultiSolutionSolver(solutionFinder)
+		solver.newSolutionCollections = MagicMock(return_value=collection)
+		collection.result = MagicMock(return_value=collectedResult)
+
+		result = solver.solve(puzzle)
+
+		self.assertEquals(result, collectedResult)
+		solver.newSolutionCollections.assert_called_once_with()
+		collection.result.assert_called_once_with()
+		solutionFinder.solve.assert_called_once_with(puzzle, collection)
+
+class TestMultiSolutionCollector(unittest.TestCase):
+	def setUp(self):
+		self.collector = MultisolutionCollector()
+		self.puzzle_1 = MockObject()
+		self.puzzle_2 = MockObject()
+
+		self.puzzle_1.compare = MagicMock(name="puzzle_1.compare", return_value = (0, 1))
+	
+	def test_OnlyOneSolutionAvailable(self):
+		self.collector.record(self.puzzle_1)
+		self.assertEquals(1, self.collector.result().solutionCount())
+
+	def test_OnlyTwoSolutionAvailable(self):
+		self.collector.record(self.puzzle_1)
+		self.collector.record(self.puzzle_2)
+
+		result = self.collector.result()
+
+		self.puzzle_1.compare.assert_called_once_with(self.puzzle_2)
+		self.assertEquals(2, result.solutionCount())
+		self.assertEquals((0, 1), result.solutionDifference())
+
+	def test_solultionCollectingNotDoneEvenAfterOneSolutionFind(self):
+		self.collector.record(self.puzzle_1)
+		self.assertFalse(self.collector.done())
+
+	def test_solutionCollectingDoneAfter2SolutionsFound(self):
+		self.collector.record(self.puzzle_1)
+		self.collector.record(self.puzzle_2)
+		self.assertTrue(self.collector.done())
+
