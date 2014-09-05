83b54b13a3058dba00986ec329a4cdbd827389d4
xuelan.mei <sharonmei1219@gmail.com>
2014-09-05 10:50:17 +0800

    remove unnecessary file

--- src/holeDigger.py
+++
@@ -1,9 +0,0 @@
-class HoleDigger:
-	def dig(self, table, puzzle, holeCount):
-		pos = puzzle.numberedPos()
-		posWithHole = self.randomlyRemoveOne(pos)
-		result = self.solve(table, puzzle)
-		if result.solutionCount() is 1:
-			return self.createPuzzle(table, posWithHole)
-	pass
-		
